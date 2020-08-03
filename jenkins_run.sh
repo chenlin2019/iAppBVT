c9dbce7b3f63f4ea9d8e28624ddac332aae6744e
sublum <sublum@126.com>
2020-08-04 01:49:16 +0800

    增加sh运行脚本

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -1,4 +1,4 @@
-pwd=$`pwd`
+pwd=`pwd`
 echo=$pwd
 apk=$pwd\..\..\Android_build\AndroidSampleApp\app\build\outputs\apk\debug\app-debug.apk
 echo=$apk
@@ -8,5 +8,4 @@ echo=$apk
     echo 'no package on the devices'
 }
 adb install $apk
-cd iAppBVT
 mvn clean install -Dtest=AppsFlyerBVT
