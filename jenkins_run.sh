b86249531602b270967ef40c45ac032842b1a924
sublum <sublum@126.com>
2020-08-04 02:30:20 +0800

    增加sh运行脚本

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -9,16 +9,17 @@ export pwd=`pwd`
 echo $pwd
 export apk=$pwd/Android_build/AndroidSampleApp/app/build/outputs/apk/debug/app-debug.apk
 echo $apk
+export unpackage="uninstall com.appsflyer.androidsampleapp"
 
 #D:/adb/adb.exe: error while loading shared libraries: ?: cannot open shared object file: No such file or directory
 {
-  /d/adb/adb uninstall com.appsflyer.androidsampleapp
+  /d/adb/adb ${unpackage}
 } ||{
     echo 'no package on the devices'
 }
 
 
-/d/adb/adb install $apk
+/d/adb/adb install ${apk}
 
 cd iAppBVT/iAppBVT/
 
