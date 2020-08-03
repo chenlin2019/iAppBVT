ef97fccb470146a084facf7fbf621af7d85607b9
sublum <sublum@126.com>
2020-08-04 02:02:55 +0800

    增加sh运行脚本

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -1,11 +1,20 @@
+
+
+
+#/c/Users/chenl/.jenkins/workspace/iAppBVT/iAppBVT
+
+cd ..
+cd ..
 pwd=`pwd`
 echo $pwd
-apk=$pwd\..\..\Android_build\AndroidSampleApp\app\build\outputs\apk\debug\app-debug.apk
+apk=$pwd\Android_build\AndroidSampleApp\app\build\outputs\apk\debug\app-debug.apk
 echo $apk
 {
-  D:\Program Files\android-sdk_r25.2.5-windows\platform-tools\adb.exe uninstall com.appsflyer.androidsampleapp
+  \c\"Program Files"\android-sdk_r25.2.5-windows\platform-tools\adb.exe uninstall com.appsflyer.androidsampleapp
 } ||{
     echo 'no package on the devices'
 }
-D:\Program Files\android-sdk_r25.2.5-windows\platform-tools\adb.exe install $apk
+ \c\"Program Files"\android-sdk_r25.2.5-windows\platform-tools\adb.exe install $apk
+
+
 mvn clean install -Dtest=AppsFlyerBVT
