ea25c5bed4b4f4d839d2380b794b0dc8de5433b4
sublum <sublum@126.com>
2020-08-04 01:54:46 +0800

    增加sh运行脚本

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -1,11 +1,11 @@
 pwd=`pwd`
-echo=$pwd
+echo $pwd
 apk=$pwd\..\..\Android_build\AndroidSampleApp\app\build\outputs\apk\debug\app-debug.apk
-echo=$apk
+echo $apk
 {
-  adb uninstall com.appsflyer.androidsampleapp
+  D:\Program Files\android-sdk_r25.2.5-windows\platform-tools\adb.exe uninstall com.appsflyer.androidsampleapp
 } ||{
     echo 'no package on the devices'
 }
-adb install $apk
+D:\Program Files\android-sdk_r25.2.5-windows\platform-tools\adb.exe install $apk
 mvn clean install -Dtest=AppsFlyerBVT
